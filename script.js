34b885d2c43084cb67868b787fbef58900c8888f
강현구 <119887373+ardkyer@users.noreply.github.com>
2024-05-09 19:38:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -38,19 +38,30 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
-    const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new
-            faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
-            .withFaceExpressions()
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    }, 100)
-})
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  const detectFaces = async () => {
+    if (video.paused || video.ended) {
+      return;
+    }
+
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+
+    if (video.width > 0 && video.height > 0) {
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    }
+
+    requestAnimationFrame(detectFaces);
+  };
+
+  detectFaces();
+});
 
