d7ac88564ced1fc2a226a5aaf77a98b9d6cdcae9
강현구 <119887373+ardkyer@users.noreply.github.com>
2024-05-09 14:52:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 const video = document.getElementById('video');
-const expressionOutput = document.getElementById('expression-output');
-
 startVideo();
-
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/tiny_face_detector_model'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_landmark_68_model'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model'),
-  faceapi.nets.faceExpressionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_expression_model')
+faceapi.nets.tinyFaceDetector.loadFromUri('https://ardkyer.github.io/MATCH\_CV/models/tiny\_face\_detector\_model'),
+faceapi.nets.faceLandmark68Net.loadFromUri('https://ardkyer.github.io/MATCH\_CV/models/face\_landmark\_68\_model'),
+faceapi.nets.faceRecognitionNet.loadFromUri('https://ardkyer.github.io/MATCH\_CV/models/face\_recognition\_model'),
+faceapi.nets.faceExpressionNet.loadFromUri('https://ardkyer.github.io/MATCH\_CV/models/face\_expression\_model')
 ])
-  .then(startVideo)
-  .catch((error) => {
-    console.error('Error loading models:', error);
-  });
-
+.then(startVideo)
+.catch((error) => {
+console.error('Error loading models:', error);
+});
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  );
+navigator.getUserMedia(
+{ video: {} },
+stream => video.srcObject = stream,
+err => console.error(err)
+);
 }
-
 video.addEventListener('play', () => {
-  const detectFaces = async () => {
-    if (video.paused || video.ended) {
-      return;
-    }
-
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-
-    if (detections.length > 0) {
-      const expressions = detections[0].expressions;
-      const maxExpression = Object.keys(expressions).reduce((a, b) => expressions[a] > expressions[b] ? a : b);
-      const expressionValue = expressions[maxExpression].toFixed(2);
-      expressionOutput.textContent = `${maxExpression} (${expressionValue})`;
-    } else {
-      expressionOutput.textContent = '';
-    }
-
-    requestAnimationFrame(detectFaces);
-  };
-
-  detectFaces();
+const canvas = faceapi.createCanvasFromMedia(video);
+document.body.append(canvas);
+const displaySize = { width: video.width, height: video.height };
+faceapi.matchDimensions(canvas, displaySize);
+const detectFaces = async () => {
+if (video.paused || video.ended) {
+return;
+}
+const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+if (video.width > 0 && video.height > 0) {
+const resizedDetections = faceapi.resizeResults(detections, displaySize);
+canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+faceapi.draw.drawDetections(canvas, resizedDetections);
+faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+}
+requestAnimationFrame(detectFaces);
+};
+detectFaces();
 });
