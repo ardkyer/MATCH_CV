8f8854a3b930ceeb86453481b2cea86db2fbc523
강현구 <119887373+ardkyer@users.noreply.github.com>
2024-05-09 20:11:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ Promise.all([
 
   faceapi.nets.tinyFaceDetector.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/tiny_face_detector_model-shard1/tiny_face_detector_model-weights_manifest.json'),
   faceapi.nets.faceExpressionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_landmark_68_model-shard1/face_landmark_68_model-weights_manifest.json'),
-  faceapi.nets.tinyFaceDetector.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model-shard1/face_recognition_model-weights_manifest.json'),
-  faceapi.nets.tinyFaceDetector.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model-shard2/face_recognition_model-weights_manifest.json'),
-  faceapi.nets.tinyFaceDetector.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_expression_model-shard1/face_expression_model-weights_manifest.json'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model-shard1/face_recognition_model-weights_manifest.json'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model-shard2/face_recognition_model-weights_manifest.json'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_expression_model-shard1/face_expression_model-weights_manifest.json'),
 ])
 .then(startVideo)
 .catch((error) => {
