749cc41e080ce6616baeed93236c7925afb64e2f
강현구 <119887373+ardkyer@users.noreply.github.com>
2024-05-09 23:03:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model'),
   faceapi.nets.faceExpressionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_expression_model')
 ])
-.then(startVideo)
-.catch((error) => {
-  console.error('Error loading models:', error);
-});
+  .then(startVideo)
+  .catch((error) => {
+    console.error('Error loading models:', error);
+  });
 
 function startVideo() {
   navigator.getUserMedia(
@@ -22,6 +22,7 @@ function startVideo() {
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
+
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize);
 
@@ -31,7 +32,6 @@ video.addEventListener('play', () => {
     }
 
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
     if (video.width > 0 && video.height > 0) {
       const resizedDetections = faceapi.resizeResults(detections, displaySize);
@@ -40,9 +40,9 @@ video.addEventListener('play', () => {
       faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
       faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
     }
-    
+
     requestAnimationFrame(detectFaces);
   };
 
   detectFaces();
-}, 100);
+});
