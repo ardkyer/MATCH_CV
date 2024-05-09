26dbbce0a252d313247e5054865d7748b3599e69
강현구 <119887373+ardkyer@users.noreply.github.com>
2024-05-09 19:51:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
 const video = document.getElementById('video');
 
-function loadModel(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject(new Error('Failed to load model: ' + url));
-        }
-      }
-    };
-    xhr.send();
-  });
-}
-
 Promise.all([
-  faceapi.loadTinyFaceDetectorModel('https://raw.githubusercontent.com/ardkyer/MATCH_CV/main/models/tiny_face_detector_model-weights_manifest.json'),
-  faceapi.loadFaceLandmarkModel('https://raw.githubusercontent.com/ardkyer/MATCH_CV/main/models/face_landmark_68_model-weights_manifest.json'),
-  faceapi.loadFaceRecognitionModel('https://raw.githubusercontent.com/ardkyer/MATCH_CV/main/models/face_recognition_model-weights_manifest.json'),
-  faceapi.loadFaceExpressionModel('https://raw.githubusercontent.com/ardkyer/MATCH_CV/main/models/face_expression_model-weights_manifest.json'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/tiny_face_detector_model-shard1'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_landmark_68_model-shard1'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model-shard1'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_recognition_model-shard2'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://ardkyer.github.io/MATCH_CV/models/face_expression_model-shard1')
 ])
 .then(startVideo)
 .catch((error) => {
@@ -39,7 +23,7 @@ function startVideo() {
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
-
+  
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize);
 
@@ -49,7 +33,7 @@ video.addEventListener('play', () => {
     }
 
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-
+    
     if (video.width > 0 && video.height > 0) {
       const resizedDetections = faceapi.resizeResults(detections, displaySize);
       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
@@ -63,4 +47,3 @@ video.addEventListener('play', () => {
 
   detectFaces();
 });
-
